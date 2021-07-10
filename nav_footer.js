46380d7d97ab9b07f534ae77bb21046e50c54d64
saitejavarma-7 <66530030+saitejavarma-7@users.noreply.github.com>
2021-07-10 14:19:59 +0530

    Basic files

---
+++ nav_footer.js
@@ -0,0 +1,23 @@
+// this function is to make tabs active on click
+function clicked(event){
+    var elements= ['home', 'blog']
+    console.log(event);
+    for (var i=0; i<elements.length; i++){
+      var elem= document.getElementById(elements[i]);
+      if (elements[i]===event){
+        elem.className+=" active";
+      }
+      else{
+        elem.classList.remove("active");
+      }
+    }
+}
+
+// temporary
+// function openNav() {
+//     document.getElementById("mySidenav").style.width = "250px";
+// }
+
+// function closeNav() {
+//     document.getElementById("mySidenav").style.width = "0";
+// }
